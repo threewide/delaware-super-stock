f65e3528848e9b599f81e6b74bc76864966a442d
Justin Collver <collver_99@hotmail.com>
2022-09-15 23:01:54 -0400

    fixed * route

--- server.js
+++ server.js
@@ -11,12 +11,6 @@ const app = express();
 // serves css and js files from public directory
 app.use(express.static('public'));
 
-// add a static route for index.html
-app.get('/', (req, res) => {
-    // send index.html file
-    res.sendFile(__dirname + '/public/index.html');
-});
-
 // add a static route for notes.html
 app.get('/notes', (req, res) => {
     // send notes.html file
@@ -26,6 +20,20 @@ app.get('/notes', (req, res) => {
 // GET request for notes
 app.get('/api/notes', (req, res) => {
     res.json(db);
+});
+
+app.post('/api/notes', (req, res) => {
+    // Inform the client that their POST request was received
+    res.json(`${req.method} request received to upvote`);
+
+    // Log our request to the terminal
+    console.info(`${req.method} request received to upvote`);
+});
+
+// add a static route for index.html
+app.get('*', (req, res) => {
+    // send index.html file
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 // listening on PORT
